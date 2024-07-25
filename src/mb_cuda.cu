f10112316ae065b2d6fb0345cf3d6a120c37ecd3
Ty Butler <tytbu@tytbutler.com>
2024-07-25 17:38:39 -0500

    finish cuda

--- src/mb_cuda.cu
+++ src/mb_cuda.cu
@@ -3,6 +3,7 @@
 #include <exception>
 #include <cuda/std/complex>
 #include <stdexcept>
+#include <print>
 #define proportion_curve(in) in
 
 #define cu_assert(val) { cu_err((val), __FILE__, __LINE__); }
@@ -16,8 +17,8 @@ inline void cu_err(cudaError_t code, const char* file, int line) {
 typedef cuda::std::complex<float> Complex;
 
 __device__ Complex screen_space_to_complex(const int x, const int y, const int x_c, const int y_c, const float x_fc, const float y_fc, const float scale) {
-    float re = (x - x_c) / scale + x_fc;
-    float im = (y - y_c) / scale + y_fc;
+    float re = ((x - x_c) / scale) + x_fc;
+    float im = ((y - y_c) / scale) + y_fc;
     return Complex{ re, im };
 }
 
@@ -32,11 +33,14 @@ __device__ float do_mb(const Complex p, const int n_iters) {
     return proportion_curve((float) iters / n_iters);
 }
 
-__global__ void mb_kernel(float* data, int cx, int cy, float fcx, float fcy, float scale, int n_iters, int n_rows) {
+__global__ void mb_kernel(float* data, int cx, int cy, float fcx, float fcy, float scale, int n_iters, int n_rows, int n_cols) {
     int r = blockIdx.y * blockDim.y + threadIdx.y;
     int c = blockIdx.x * blockDim.x + threadIdx.x;
-    data[r*n_rows + c] = do_mb(screen_space_to_complex(c, r, cx, cy, fcx, fcy, scale), n_iters);
+    if (r < n_rows && c < n_cols) {
+	data[r*n_cols + c] = do_mb(screen_space_to_complex(c, r, cx, cy, fcx, fcy, scale), n_iters);
+    }
 }
+
 
 void calculate_mb(std::vector<std::vector<float>>& in_v, const Point center, const float scale, const int n_iters) {
     int n_rows = in_v.size();
@@ -55,10 +59,12 @@ void calculate_mb(std::vector<std::vector<float>>& in_v, const Point center, con
     dim3 threadsPerBlock(16, 16);
     dim3 numBlocks(n_cols / threadsPerBlock.x, n_rows / threadsPerBlock.y);
 
-    mb_kernel<<<numBlocks, threadsPerBlock>>>(dev_data, x_c, y_c, center.x, center.y, scale, n_iters, n_rows);
+    mb_kernel<<<numBlocks, threadsPerBlock>>>(dev_data, x_c, y_c, center.x, center.y, scale, n_iters, n_rows, n_cols);
+
+
     cu_assert(cudaDeviceSynchronize());
     for (int r = 0; r < n_rows; r++) {
-	cu_assert(cudaMemcpy(in_v[r].data(), dev_data + r * n_cols, n_cols, cudaMemcpyDeviceToHost));
+	cu_assert(cudaMemcpy(in_v[r].data(), dev_data + r * n_cols, n_cols * sizeof(float), cudaMemcpyDeviceToHost));
     }
     cu_assert(cudaFree(dev_data));
 }
